8e32c469f38456959443c33bfeefece3cd4c7cf3
Bihotxi <roberpdlcruz@hotmail.com>
2022-07-25 20:30:11 +0200

    Edit CSS for new changes. Add dark theme to CSS. Finish and implement list of elements. Clean html file

--- index.js
+++ index.js
@@ -6,129 +6,153 @@ let holidayToDoListComplete = [];
 
 const allElementsList = localStorage.getItem("items");
 if ( allElementsList !== null) {
-holidayToDoList = JSON.parse(allElementsList );
-
+  holidayToDoList = JSON.parse(allElementsList );  
 }
 
 const completedElementsList = localStorage.getItem("itemsCompleted");
 if ( completedElementsList !== null) {
-holidayToDoListComplete = JSON.parse(completedElementsList );
-
-}
+  holidayToDoListComplete = JSON.parse(completedElementsList );  
+} 
 
 const input = document.getElementById("toDo-input") 
-// const button = document.getElementById("add-Button") 
 input.onchange = addItem;
 
 function addItem() {
   const toDo = input.value;  
   holidayToDoList.push(toDo);
+  localStorage.setItem("items", JSON.stringify(holidayToDoList))
   renderItems();
-  input.value = "";
-   
+  input.value = "";   
 } 
 
 function uncheckToDoElement(i) {
   holidayToDoListComplete.splice(i, 1);
-  localStorage.setItem("itemsCompleted", JSON.stringify(holidayToDoListComplete))
-  // localStorage.setItem("items", JSON.stringify(holidayToDoList))
+  localStorage.setItem(
+    "itemsCompleted",
+    JSON.stringify(holidayToDoListComplete)
+  );
 }
-
-function deleteToDoElement(i) {
-  holidayToDoList.splice(i, 1);
-  holidayToDoListComplete.splice(i, 1);
-  localStorage.removeItem(i);
-  renderItems();
-}
-
 
 function toggleCheck(event) {
   const targetElement = event.target;
   const targetValue = targetElement.value;
   const targetParent = targetElement.parentNode;
-  const list = document.getElementById("list");
-  const listCompleted = document.getElementById("list-Completed");
 
   if (targetElement.checked) {
     targetParent.id = "completed";
-    list.insertBefore(targetParent, list.children[-1]);
     holidayToDoListComplete.push(targetValue);
-    listCompleted.appendChild(targetParent);
-    localStorage.setItem("itemsCompleted", JSON.stringify(holidayToDoListComplete))         
-    
-    
-  } else {
+    localStorage.setItem("itemsCompleted", JSON.stringify(holidayToDoListComplete));
+    } else {
     const targetIndex = holidayToDoListComplete.indexOf(
       targetValue
-      );
-      console.log("Valor de target",targetValue );
+    );
     targetParent.id = "";
-    uncheckToDoElement(targetIndex);
-    
-    list.appendChild(targetParent);
+    uncheckToDoElement(targetIndex);    
+  }
+  renderItems();
+}
+
+function deleteToDoElement(generalIndex, completedIndex) {
+  holidayToDoList.splice(generalIndex, 1);
+  localStorage.setItem("items",JSON.stringify(holidayToDoList));
+  
+  if (completedIndex >= 0) {
+    holidayToDoListComplete.splice(completedIndex, 1);
+    localStorage.setItem("itemsCompleted", JSON.stringify(holidayToDoListComplete));
   }
 }
 
+function handleDelete(itemValue) {
+  const generalIndex = holidayToDoList.indexOf(
+    itemValue
+  );
+  const completedIndex = holidayToDoListComplete.indexOf(
+    itemValue
+  );
+  deleteToDoElement(generalIndex, completedIndex);  
+  renderItems();
+}
+
+function generateItem(itemValue) {
+  const listElement = document.createElement("li");
+  listElement.className = "newToDo";      
+  
+  const checkInput = document.createElement("input");    
+  checkInput.classList = "check";
+  checkInput.id = "check";
+  checkInput.value = itemValue;
+  listElement.appendChild(checkInput).type = "checkbox";      
+  
+  const labelToDoEntry = document.createElement("label"); 
+  labelToDoEntry.setAttribute("for", "check");
+  const labelToDoText = document.createTextNode(itemValue);
+  labelToDoEntry.appendChild(labelToDoText);
+  listElement.appendChild(labelToDoEntry);      
+  
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete-button"
+  deleteButton.id = "delete-button"
+  deleteButton.textContent = "X";
+  listElement.appendChild(deleteButton);
+  deleteButton.onclick = () => {
+      handleDelete(itemValue);
+  };
+  return { listElement, checkInput};
+}
+
 function renderItems() {
+  const list = document.getElementById("list");
+  const listCompleted = document.getElementById("list-Completed")
+
   list.innerHTML = "";  
-
-  for (const i in holidayToDoList) {
-    const itemValue = holidayToDoList[i];
+  listCompleted.innerHTML = ""
+  
+  for (const generalIndex in holidayToDoList) {
+    const itemValue = holidayToDoList[generalIndex];
+    const { listElement, checkInput} = generateItem(itemValue)
+    
     const isACompletedItem = holidayToDoListComplete.find((item) => item === itemValue);
-    const toDoEntry = document.createElement("li");
-    toDoEntry.className = "newToDo";
-    // const newToDoText = document.createTextNode(itemValue);
-    // toDoEntry.appendChild(newToDoText);
-    
-    
-    
-    const checkToDoEntry = document.createElement("input");    
-    checkToDoEntry.classList = "check";
-    checkToDoEntry.id = "check";
-    checkToDoEntry.value = itemValue;
-    toDoEntry.appendChild(checkToDoEntry).type = "checkbox";
-
-    if (isACompletedItem) {
-      toDoEntry.id = "completed";
-      checkToDoEntry.checked = true;
-      
-            //  console.log("Lista de tareas completadas:", holidayToDoListComplete);
+    if (!isACompletedItem) {
+      list.appendChild(listElement);      
     }
+    checkInput.addEventListener("change", toggleCheck);
+  }  
+ 
+  for (const completedIndex in holidayToDoListComplete) {
+    const itemValue = holidayToDoListComplete[completedIndex];
+    const { listElement, checkInput} = generateItem(itemValue) 
+    listElement.id = "completed"
+    checkInput.checked = true;    
+    
+    listCompleted.appendChild(listElement)
+    checkInput.addEventListener("change", toggleCheck);
+  }  
+  const totalSpan = document.getElementById("completed-total");
+  const accordionButton = document.getElementById("accordion-button");
+  totalSpan.innerHTML = `${holidayToDoListComplete.length} elementos completados`;
 
-    const labelToDoEntry = document.createElement("label"); 
-    labelToDoEntry.setAttribute("for", checkToDoEntry)
-    const labelToDoText = document.createTextNode(itemValue);
-    toDoEntry.appendChild(labelToDoText)
-
-
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "delete-button"
-    deleteButton.id = "delete-button"
-    deleteButton.textContent = "X";
-    toDoEntry.appendChild(deleteButton);
-    deleteButton.onclick = () => {
-      deleteToDoElement(i);
-    };
-    
-    list.appendChild(toDoEntry);
-    
-    console.log("Entrada toDoEntry:", toDoEntry);
-    localStorage.setItem("items", JSON.stringify(holidayToDoList))
-    
-    
-    
-    checkToDoEntry.addEventListener("change", toggleCheck);
-  }
+  accordionButton.onclick = () => {
+    listCompleted.classList.toggle("countTextVisibility");
+  };
+  
   console.log("Valor de items completados:", holidayToDoListComplete);
-  console.log("Valor de la lista de items:", holidayToDoList);
+  console.log("Valor de la lista de items:", holidayToDoList);  
 } 
 renderItems()
 
 
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+darkModeToggle.innerText = "Modo noche"
+const body = document.getElementById("body")
 
-
-  
-
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle("darkmode");
+  localStorage.setItem("darkmode", body.classList);
+  });
+window.onload = () => {
+  const themeClass = localStorage.getItem("darkmode");
+  body.classList = themeClass;
+}
 
 
 

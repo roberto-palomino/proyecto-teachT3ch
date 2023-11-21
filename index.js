d21c4dc4b276711407fa1169be88ff14998f6d89
roberto-palomino <roberpdlcruz@hotmail.com>
2023-11-21 20:29:00 +0100

    fix: modified code to solve error in theme button text

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-'use strict'
+"use strict";
 
 let holidayToDoList = [];
 let holidayToDoListComplete = [];
 
 const allElementsList = localStorage.getItem("items");
-if ( allElementsList !== null) {
-  holidayToDoList = JSON.parse(allElementsList );  
+if (allElementsList !== null) {
+  holidayToDoList = JSON.parse(allElementsList);
 }
 const completedElementsList = localStorage.getItem("itemsCompleted");
-if ( completedElementsList !== null) {
-  holidayToDoListComplete = JSON.parse(completedElementsList );  
-} 
+if (completedElementsList !== null) {
+  holidayToDoListComplete = JSON.parse(completedElementsList);
+}
 
-const input = document.getElementById("toDo-input") 
+const input = document.getElementById("toDo-input");
 input.onchange = addItem;
 
 function addItem() {
-  const toDo = input.value;  
+  const toDo = input.value;
   holidayToDoList.push(toDo);
-  localStorage.setItem("items", JSON.stringify(holidayToDoList))
+  localStorage.setItem("items", JSON.stringify(holidayToDoList));
   renderItems();
-  input.value = "";   
-} 
+  input.value = "";
+}
 
 function uncheckToDoElement(i) {
   holidayToDoListComplete.splice(i, 1);
@@ -39,64 +39,63 @@ function toggleCheck(event) {
   if (targetElement.checked) {
     targetParent.id = "completed";
     holidayToDoListComplete.push(targetValue);
-    localStorage.setItem("itemsCompleted", JSON.stringify(holidayToDoListComplete));
-    } else {
-    const targetIndex = holidayToDoListComplete.indexOf(
-      targetValue
+    localStorage.setItem(
+      "itemsCompleted",
+      JSON.stringify(holidayToDoListComplete)
     );
+  } else {
+    const targetIndex = holidayToDoListComplete.indexOf(targetValue);
     targetParent.id = "";
-    uncheckToDoElement(targetIndex);    
+    uncheckToDoElement(targetIndex);
   }
   renderItems();
 }
 
 function deleteToDoElement(generalIndex, completedIndex) {
   holidayToDoList.splice(generalIndex, 1);
-  localStorage.setItem("items",JSON.stringify(holidayToDoList));
-  
+  localStorage.setItem("items", JSON.stringify(holidayToDoList));
+
   if (completedIndex >= 0) {
     holidayToDoListComplete.splice(completedIndex, 1);
-    localStorage.setItem("itemsCompleted", JSON.stringify(holidayToDoListComplete));
+    localStorage.setItem(
+      "itemsCompleted",
+      JSON.stringify(holidayToDoListComplete)
+    );
   }
 }
 
 function handleDelete(itemValue) {
-  const generalIndex = holidayToDoList.indexOf(
-    itemValue
-  );
-  const completedIndex = holidayToDoListComplete.indexOf(
-    itemValue
-  );
-  deleteToDoElement(generalIndex, completedIndex);  
+  const generalIndex = holidayToDoList.indexOf(itemValue);
+  const completedIndex = holidayToDoListComplete.indexOf(itemValue);
+  deleteToDoElement(generalIndex, completedIndex);
   renderItems();
 }
 
 function generateItem(itemValue) {
   const listElement = document.createElement("li");
-  listElement.className = "newToDo";      
-  
-  const checkInput = document.createElement("input");    
+  listElement.className = "newToDo";
+
+  const checkInput = document.createElement("input");
   checkInput.classList = "check";
-  // checkInput.id = "check";
   checkInput.value = itemValue;
-  listElement.appendChild(checkInput).type = "checkbox";      
-  
+  listElement.appendChild(checkInput).type = "checkbox";
+
   const labelToDoEntry = document.createElement("label");
   labelToDoEntry.classList = "label-input";
   labelToDoEntry.setAttribute("for", "check");
   const labelToDoText = document.createTextNode(itemValue);
   labelToDoEntry.appendChild(labelToDoText);
-  listElement.appendChild(labelToDoEntry);      
-  
+  listElement.appendChild(labelToDoEntry);
+
   const deleteButton = document.createElement("button");
-  deleteButton.className = "delete-button"
-  deleteButton.id = "delete-button"
-  deleteButton.textContent = "X";  
+  deleteButton.className = "delete-button";
+  deleteButton.id = "delete-button";
+  deleteButton.textContent = "X";
   listElement.appendChild(deleteButton);
   deleteButton.onclick = () => {
-      handleDelete(itemValue);
+    handleDelete(itemValue);
   };
-  return { listElement, checkInput};
+  return { listElement, checkInput };
 }
 
 function renderItems() {
@@ -133,48 +132,47 @@ function renderItems() {
   const menuAccordion = document.getElementById("menu-accordion-button");
   const accordionButton = document.getElementById("completed-icon");
   totalSpan.innerText = `${holidayToDoListComplete.length} elementos completados`;
-  // accordionButton.innerText = "expand_more";
   menuAccordion.onclick = () => {
     listCompleted.classList.toggle("countTextVisibility");
-    
+
     if (accordionButton.innerText === "expand_more") {
       accordionButton.innerText = "chevron_right";
     } else {
       accordionButton.innerText = "expand_more";
     }
-   
-  }; 
+  };
 }
 
 renderItems();
-    
 
 const darkModeToggle = document.getElementById("dark-mode-toggle");
-darkModeToggle.textContent = "Dark Theme";
 const body = document.getElementById("body");
-darkModeToggle.addEventListener('click', () => {
-  
-  body.classList.toggle("darkmode");
-  localStorage.setItem("darkmode", body.classList);
- 
-  if (body.className === "darkmode") {
-    darkModeToggle.textContent = "Light Theme"
+
+function updateButtonText() {
+  if (body.classList.contains("darkmode")) {
+    darkModeToggle.textContent = "Light Theme";
   } else {
-    darkModeToggle.textContent = "Dark Theme"
+    darkModeToggle.textContent = "Dark Theme";
   }
-  localStorage.setItem("theme-mode", darkModeToggle.textContent);
-  });
-window.onload = () => {
-  const themeClass = localStorage.getItem("darkmode");
-  body.classList = themeClass;
-  const themeMode = localStorage.getItem("theme-mode");
-  darkModeToggle.textContent = themeMode;
 }
 
+darkModeToggle.addEventListener("click", () => {
+  body.classList.toggle("darkmode");
+  localStorage.setItem("darkmode", body.classList);
+  updateButtonText();
+  localStorage.setItem("theme-mode", darkModeToggle.textContent);
+});
 
+window.onload = () => {
+  const themeClass = localStorage.getItem("darkmode");
 
+  if (themeClass) {
+    body.classList = themeClass;
+  }
+  const themeMode = localStorage.getItem("theme-mode");
 
-
-
-    
-  
+  if (themeMode) {
+    darkModeToggle.textContent = themeMode;
+  }
+  updateButtonText();
+};
